7ad10a41686370dd7213a15c3701eeb3b29fe16e
anishbista92 <anishbista9235@gmail.com>
2023-12-13 20:55:00 +0545

    Task 2 updated

--- book_author/book/models.py
+++ book_author/book/models.py
@@ -5,14 +5,24 @@ class Author(models.Model):
     name = models.CharField(max_length=100)
     bio = models.TextField()
 
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Author"
+        verbose_name_plural = "Authors"
+
     def __str__(self):
         return self.name
 
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
     published_date = models.DateField()
+
+    class Meta:
+        ordering = ["title"]
+        verbose_name = "Book"
+        verbose_name_plural = "Books"
 
     def __str__(self):
         return self.title
